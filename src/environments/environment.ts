e0bd5726b0cddd6b3dc02f9d00cd08a4bfbaa33a
Rosalise <kapsophia@gmail.com>
2023-05-15 00:26:07 +0300

    Updated firebase database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
-    firebaseConfig : {
-        apiKey: "AIzaSyC5OyHw9_UGlmWyTJ-yjHMEllo9vUnehCI",
-        authDomain: "notes-app-c05e3.firebaseapp.com",
-        projectId: "notes-app-c05e3",
-        storageBucket: "notes-app-c05e3.appspot.com",
-        messagingSenderId: "1009604441606",
-        appId: "1:1009604441606:web:7d4bc9765609abb41df587"
-      },
-    production: false
-  };
+  firebaseConfig: {
+    apiKey: "AIzaSyCJUSYbmPyYgdT7_AHiEq7-OWyo_xKxzbU",
+    authDomain: "notes-app-3d1b3.firebaseapp.com",
+    projectId: "notes-app-3d1b3",
+    storageBucket: "notes-app-3d1b3.appspot.com",
+    messagingSenderId: "59314130328",
+    appId: "1:59314130328:web:39b6077dfc4298dd1e2fed",
+    measurementId: "G-RF3LNXDSYX",
+  },
+  production: false,
+};
